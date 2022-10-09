9e3809ed6e9cf0228d0903f347b5cd628d07693c
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2022-10-09 20:56:24 +0200

    Added a brand new instance system with a null check.
    Added a utility class for translating color codes to the minecraft-accepted format.

---
+++ src/main/java/me/xemu/youtubecore/Utils.java
@@ -0,0 +1,19 @@
+package me.xemu.youtubecore;
+
+import org.bukkit.ChatColor;
+
+/**
+ * YouTubeCore - Created by Xemu
+ * Redistribution of this Project is not allowed
+ *
+ * @author Xemu
+ * Created At: 09.10.2022
+ * Project: YouTubeCore
+ */
+public class Utils {
+
+	public static String translate(String str) {
+		return ChatColor.translateAlternateColorCodes('&', str);
+	}
+
+}
--- src/main/java/me/xemu/youtubecore/YouTubeCore.java
+++ src/main/java/me/xemu/youtubecore/YouTubeCore.java
@@ -2,16 +2,25 @@ package me.xemu.youtubecore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class YouTubeCore extends JavaPlugin {
+public class YouTubeCore extends JavaPlugin {
+
+	private static YouTubeCore instance;
 
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
-
+		instance = this;
 	}
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
+		instance = null;
+	}
+
+	public static YouTubeCore getInstance() {
+		if (instance == null) {
+			throw new NullPointerException("The instance is not yet valid. That means that getInstance() has been called" +
+					"before the plugin starts.");
+		}
+		return instance;
 	}
 }
